0304a91f7d07a25373ec748cefe89682c03c213c
Santiago Madketing <santiago@madketing.com.ar>
2019-05-20 13:52:07 -0300

    Last Changes

--- content.php
+++ content.php
@@ -15,23 +15,33 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $check_in_format = date("Y-m-d", strtotime($check_in));
     $check_out_format = date("Y-m-d", strtotime($check_out));
 
-    curl_setopt($ch, CURLOPT_URL,"https://beta.id90travel.com/api/v1/hotels");
-    curl_setopt($ch, CURLOPT_POSTFIELDS, "destination=".$destination."&checkin=".$check_in_format."&checkout=".$check_out_format."&guests[]=".$guests);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+    $params = "destination=".$destination."&checkin=".$check_in_format."&checkout=".$check_out_format."&guests[]=".$guests;
 
+    curl_setopt_array($ch, array(
+        CURLOPT_URL => "https://beta.id90travel.com/api/v1/hotels?".$params,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => "",
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 30,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => "GET",
+        CURLOPT_POSTFIELDS => "",
+        CURLOPT_HTTPHEADER => array(
+            "Postman-Token: 1dc75080-cab5-4dad-b0e5-71c6aca0c3f3",
+            "cache-control: no-cache"
+        ),
+    ));
 
     $hotels_response = curl_exec ($ch);
-    echo $hotels_response;
 
     $hotels_response_status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
     $hotels_response_result = json_decode($hotels_response,true);
 
-    $_SESSION["hotels_response"] = $hotels_response_result;
+    $_SESSION["hotels_response"] = $hotels_response_result["hotels"];
 
     curl_close ($ch);
 
-    echo $hotels_response_status;
+//    echo $hotels_response_status;
 }
 
 $first_name = $_SESSION["server_response"]["member"]["first_name"];
