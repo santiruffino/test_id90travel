c23107e7e85e7d60bb1280d1bbf38f7cb6d1049c
Santiago Madketing <santiago@madketing.com.ar>
2019-05-20 18:03:56 -0300

    Last Changes

--- views/show_hotels.view.php
+++ views/show_hotels.view.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css/font-awesome.min.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <link rel="icon" type="image/ico" href="img/favicon.ico"/>
 
     <title>ID90Travel</title>
@@ -23,11 +23,13 @@
                 $check_in = $valor["checkin"];
                 $check_out = $valor["checkout"];
                 $description = $valor["description"];
+                $retail_sale = $valor["retail_rate"];
 
                 echo "<div class=\"card\" style=\"width:100%\">
                     <div class=\"card-body\">
                         <h4 class=\"card-title\">$hotel_name</h4>
                             <p class=\"card-text\">$description</p>  
+                             <p class=\"card-text\">Precio minorista: $retail_sale</p>  
                     </div>
                   </div>";
             } ?>
