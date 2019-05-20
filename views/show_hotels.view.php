92ec5e137ca91ad10ee09552e9d49ae90c25a62b
Santiago Madketing <santiago@madketing.com.ar>
2019-05-20 18:00:12 -0300

    Last Changes

--- views/show_hotels.view.php
+++ views/show_hotels.view.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css/font-awesome.min.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="css/style.css">
     <link rel="icon" type="image/ico" href="img/favicon.ico"/>
 
@@ -13,8 +13,24 @@
 </head>
 
 <body class="bg-image">
-<div id="wrapwrap" class="container">
-    <div class="container">
+<div class="container">
+    <div class="row">
+        <div class="col-container">
+            <?php
+            foreach ($_SESSION["hotels_response"] as &$valor) {
+                $hotel_name = $valor["name"];
+                $image = $valor["image"];
+                $check_in = $valor["checkin"];
+                $check_out = $valor["checkout"];
+                $description = $valor["description"];
 
+                echo "<div class=\"card\" style=\"width:100%\">
+                    <div class=\"card-body\">
+                        <h4 class=\"card-title\">$hotel_name</h4>
+                            <p class=\"card-text\">$description</p>  
+                    </div>
+                  </div>";
+            } ?>
+        </div>
     </div>
-</div>
+</body>
